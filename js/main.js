9e0a75bfd3cefa2936f4e8933313795d60fc272f
Angel Leon <angel.leon.ram@gmail.com>
2023-05-20 20:57:01 -0300

    Agregamso el código para que funcione correctamente la calculadora en conjunto con la tabla, se guardan los datos en el array y en el sessionStorage y localStorage

---
+++ js/main.js
@@ -0,0 +1,211 @@
+//REFRESH DEFAULT
+window.addEventListener("load", () => {
+    let inputs = document.querySelectorAll("#table__students input");
+    inputs.forEach((input) => (input.value = ""));
+});
+
+//CONSTANTES
+const add_student = document.getElementById("table__add-student");
+const table = document.getElementById("table__students");
+const session_button = document.getElementById("btn-session");
+const local_button = document.getElementById("btn-local");
+const regex = /^[a-zA-Z\s]+$/;
+
+
+//ARREGLO PRINCIPAL
+let estudiantes = []
+
+//CLASE ALUMNO
+class Alumno {
+    constructor (nombre, nota1, nota2, nota3){
+        this.nombre = nombre;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.promedio = this.calcularPromedio();
+    }
+    //Cálculo del promedio
+    calcularPromedio(){
+        let notas = [this.nota1, this.nota2, this.nota3];
+        let promedio = notas.reduce((sum, nota) => sum + nota, 0) /notas.length;
+        return promedio.toFixed(2);
+    }
+};
+
+//EVENTOS
+//Guardar en el sessionStorage
+session_button.addEventListener("click", () => {
+    sessionStorage.setItem("estudiantes", JSON.stringify(estudiantes));
+})
+//Guardar en el localStorage
+local_button.addEventListener("click", () => {
+    localStorage.setItem("estudiantes", JSON.stringify(estudiantes));
+})
+//Agregar fila de estudiante
+add_student.addEventListener("click", () => {
+    let row_creation = document.createElement("tr");
+    row_creation.innerHTML = `
+    <td><input type="text" placeholder="Nombre del alumno"></td>
+    <td><input type="text" placeholder="Exámen 1"></td>
+    <td><input type="text" placeholder="Exámen 2"></td>
+    <td><input type="text" placeholder="Exámen 3"></td>
+    <td class="promedium">Promedio</td>
+    <td><button class="calc-button">Calcular promedio</button><td>
+    `;
+    table.append(row_creation);
+});
+
+//Asignar eventos a los botones de cálculo
+table.addEventListener("click", (event) => {
+    if (event.target.classList.contains("calc-button")){
+        let currentRow = event.target.parentNode.parentNode;
+        let inputs = currentRow.getElementsByTagName("input");
+        let nombre = inputs[0].value;
+        let nota1 = Number(inputs[1].value);
+        let nota2 = Number(inputs[2].value);
+        let nota3 = Number(inputs[3].value);
+
+        if (!validarNombre(nombre)) {
+            alert("Error: El nombre del alumno debe contener solo letras y espacios");
+            return;
+        }
+        if (!validarNota(nota1) || !validarNota(nota2) || !validarNota(nota3)) {
+            alert("Error: Las notas deben estar entre 0 y 20");
+            return;
+        }
+
+        let alumno = new Alumno(nombre, nota1, nota2, nota3);
+        estudiantes.push(alumno);
+        let promedioCell = currentRow.getElementsByClassName("promedium")[0];
+        promedioCell.innerText = alumno.promedio;
+    }
+})
+
+//FUNCIONES
+// Validar nombre
+function validarNombre(nombre) {
+    return regex.test(nombre);
+}
+
+// Validar nota
+function validarNota(nota) {
+    return !isNaN(nota) && nota >= 0 && nota <= 20;
+}
+
+//Solicitud de datos
+function dataSolicitation(){
+    let nombreAlumno = nameInput();
+    let nota_1 = pedirNota(1);
+    let nota_2 = pedirNota(2);
+    let nota_3 = pedirNota(3);
+    let promedio = calcularPromedio(nota_1, nota_2, nota_3);
+    studentAdd (nombreAlumno, nota_1, nota_2, nota_3, promedio);
+}
+//Pedir el nombre
+function nameInput(){
+    let alumnoInput = document.querySelector('input[placeholder="Nombre del alumno"]');
+    let nombre = alumnoInput.value.trim();
+    if (!regex.test(nombre)) {
+        alert("Error: El nombre del alumno debe contener solo letras y espacios")
+        return "";
+    }
+    return nombre;
+};
+//Solicitud de nota y validación
+function pedirNota(notaNum){
+    let notaInput = document.querySelector('input[placeholder = "Exámen ${notaNum}"]');
+    let nota = Number(notaInput.value.trim());
+    if (isNaN(nota) || nota < 0 || nota > 20) {
+        alert("Error: La nota debe estar entre 0 y 20");
+        return "";
+    }
+    return nota;
+};
+//Adición de datos al arreglo
+function studentAdd(nombre, nota_1, nota_2, nota_3, promedio){
+    let estudiante = {nombre: nombre, nota1: nota_1, nota2: nota_2, nota3: nota_3, promedio: promedio};
+    estudiantes.push(estudiante);
+};
+
+
+
+
+
+//
+////////////
+
+
+// //Devolución final de datos
+// const OPCION_ALUMNOS = 1   
+// const OPCION_ALUMNOS_PROMEDIOS = 2;
+// const OPCION_ALUMNOS_INGRESADOS = 3;
+// const OPCION_SALIR = 4;
+
+// switch (mostrarMenu()) {
+//     case OPCION_ALUMNOS: 
+//         namesDev();
+//         break;
+//     case OPCION_ALUMNOS_PROMEDIOS:
+//         namesProm();
+//         break;
+//     case OPCION_ALUMNOS_INGRESADOS:
+//       showData();
+//       break;
+//     case OPCION_SALIR:
+//       alert("Saliendo del programa");
+//       break;
+//     default:
+//       alert("Opción inválida");
+//       break;
+// };
+
+// //FUNCIONES DEL SWITCH
+// //CICLO PRINCIPAL
+// function mostrarMenu() {
+//     let opcion = 0;
+//     while (opcion < OPCION_ALUMNOS || opcion > OPCION_SALIR){
+//         opcion = Number(prompt(`Ingrese una opción:
+//         ${OPCION_ALUMNOS}. Mostrar nombres de los alumnos ingresados
+//         ${OPCION_ALUMNOS_PROMEDIOS}. Mostrar nombres de los alumnos y sus promedios
+//         ${OPCION_ALUMNOS_INGRESADOS}. Datos de todos los alumnos ingresados
+//         ${OPCION_SALIR}. Salir`));
+//     };
+//     return opcion;
+//   }
+
+// //FUNCIONES DE DEVOLUCIÓN
+// //Devolución de nombres
+// function namesDev(){
+//     let mensaje = estudiantes.map(estudiante => estudiante.nombre).join("\n");
+//     alert(mensaje);
+// };
+
+// //Devolución de nombres y promedios
+// function namesProm(){
+//     let mensaje = estudiantes.map(estudiante => estudiante.nombre + " ---- " + estudiante.promedio).join("\n");
+//     alert(mensaje);
+// };
+
+// //Devolucion de cada estudiante
+// function showData(){
+//     let mensaje = "";
+//     for (let i = 0; i < estudiantes.length; i++) {
+//         mensaje += "Estudiante #" + (i + 1) + ":\n" +
+//                    "Nombre: " + estudiantes[i].nombre + "\n" +
+//                    "Nota 1: " + estudiantes[i].nota1 + "\n" +
+//                    "Nota 2: " + estudiantes[i].nota2 + "\n" +
+//                    "Nota 3: " + estudiantes[i].nota3 + "\n" +
+//                    "Promedio: " + estudiantes[i].promedio + "\n\n";
+//     };
+//     alert(mensaje);
+// };
+
+
+
+// //INNECESARIO EN ESTA PREENTREGA
+// // CICLO PRINCIPAL
+// // let permiso = prompt ("Si desea ingresar las notas de un alumno, escriba 'Si'. De lo contrario, escriba cualquier letra para salir")
+// // while (permiso.toLowerCase() == "si"){
+// //     studentProm()
+// //     permiso = prompt("Si desea continuar promediando escriba `Si`, de lo contrario, escriba cualquier letra")
+// // };
