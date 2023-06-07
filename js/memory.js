b2893291d36a722889c1c6b878e8455a2ccea1e2
Angel Leon <angel.leon.ram@gmail.com>
2023-06-07 02:18:26 -0300

    Agregamos método de filtrado por promedios, agregamos un corte de intervalo para la función de las citas. Falta agregar estilos, y capaz alguna otra función

--- js/memory.js
+++ js/memory.js
@@ -1,67 +1,55 @@
+//Llamamiento del DOM y API's
 const sessionDataDiv = document.getElementById("session-storage-data");
 const localDataDiv = document.getElementById("local-storage-data");
-const finalDiv = document.getElementById("final");
-const unsplashAPI = 'https://api.unsplash.com/search/photos?query=teacher&client_id=kPRGje6wBX77MKijLObZFER5LRHPIcYknI2dkgTsiVo'
+const quotesShooter = document.getElementById("quotesShooter");
+const quotesStopper = document.getElementById("quotesStopper");
+const ordenarMejoresBtn = document.getElementById("ordenarMejores");
+const ordenarPeoresBtn = document.getElementById("ordenarPeores");
+const url = 'https://api.quotable.io/random';
 
-//SESSION STORAGE
+
+//SESSION STORAGE Llamamiento de infromación
+// Obtener los datos del Session Storage
 const sessionData = sessionStorage.getItem("estudiantesSession");
-
 // Generar contenido HTML para el Session Storage
 let sessionHTML = "<h3>Registro de Sesión</h3>";
-sessionHTML += "<ul>";
+sessionHTML += "<tr>";
 if (sessionData) {
   const estudiantes = JSON.parse(sessionData);
   estudiantes.forEach((estudiante) => {
-    sessionHTML += `<li>${estudiante.nombre} - Promedio: ${estudiante.promedio}</li>`;
+    sessionHTML += `<td class="name">${estudiante.nombre}</td>
+    <td class="promedium"> Promedio: ${estudiante.promedio}</td> 
+    </br>`;
   });
 }
-sessionHTML += "</ul>";
-
-//Se pueden ordenar mejor los datos, en forma de tabla
-
+sessionHTML += "</tr>";
 // Mostrar contenido del Session Storage
 sessionDataDiv.innerHTML = sessionHTML;
 
+
+//LOCAL STORAGE Llamamiento de infromación
 // Obtener los datos del Local Storage
 const localData = localStorage.getItem("estudiantesHistorial");
-
 // Generar contenido HTML para el Local Storage
 let localHTML = "<h3>Registro Histórico</h3>";
-localHTML += "<ul>";
+localHTML += "<tr>";
 if (localData) {
   const estudiantes = JSON.parse(localData);
   estudiantes.forEach((estudiante) => {
-    localHTML += `<li>${estudiante.nombre} - Promedio: ${estudiante.promedio}</li>`;
+    localHTML += `<td>${estudiante.nombre} </td>
+    <td> Promedio: ${estudiante.promedio} </td> 
+    </br>`;
   });
 }
-localHTML += "</ul>";
+localHTML += "</tr>";
 // Mostrar contenido del Local Storage
 localDataDiv.innerHTML = localHTML;
 
 
-// // Ordenar los estudiantes por fecha (supongamos que tienes una propiedad "fecha" en cada estudiante)
-// estudiantes.sort((a, b) => {
-//   const dateA = new Date(a.fecha);
-//   const dateB = new Date(b.fecha);
-//   return dateA - dateB;
-// });
-
-
-//fetch(unsplashAPI)
-// .then((response) => response.json())
-// .then((data) => {
-//   data.forEach((pic) => {
-//     let boxCreation = document.createElement("tr");
-//     boxCreation.textContent = usuario.name + ", Teléfono " + usuario.phone;
-//     finalDiv.append(boxCreation)
-    
-//   })
-// })
-
-//Oye profe, practica un pco tu ingles leyendo estas frases...
-
-const url = 'https://api.quotable.io/random'
-function getQuote() {
+//PROMESAS (API's) Y LIBRERÍAS
+//Llamamiento de API mediante Fetch y uso de librería
+let intervalId;
+const droppingQuotes = () => {
   fetch(url)
     .then(response => response.json())
     .then((data) => {
@@ -69,8 +57,71 @@ function getQuote() {
         text: data.content + " Author: " + data.author,
         gravity: 'bottom',
         position: 'left',
-	      duration: 8000,
+        duration: 8000,
       }).showToast();
     })
 }
-setInterval(getQuote, 9000);
+
+//Disparador de citas motivacionales en Inglés
+quotesShooter.addEventListener("click", () => {
+  intervalId = setInterval(droppingQuotes, 9000);
+});
+//Función de corte de las citas
+quotesStopper.addEventListener("click", () => {
+  clearInterval(intervalId);
+});
+
+
+//FILTRADO DE INFORMACIÓN
+// Función para filtrar y mostrar los datos del Local Storage y Session Storage
+function filtrarData(event) {
+  event.preventDefault();
+  const filtro = event.target.textContent;
+
+  // Obtener los datos
+  const localData = localStorage.getItem("estudiantesHistorial");
+  const sessionData = sessionStorage.getItem("estudiantesSession");
+
+  // Generar contenido HTML filtrado para el Local Storage
+  let localHTML = "<h3>Registro Histórico</h3>";
+  localHTML += "<ul>";
+
+  // Generar contenido HTML filtrado para el Session Storage
+  let sessionHTML = "<h3>Registro de Sesión</h3>";
+  sessionHTML += "<ul>";
+
+  if (localData) {
+    const estudiantesLocal = JSON.parse(localData);
+    const estudiantesSession = sessionData ? JSON.parse(sessionData) : [];
+
+    // Filtrar y ordenar estudiantes según el criterio seleccionado
+    let estudiantesLocalFiltrados;
+    let estudiantesSessionFiltrados;
+
+    if (filtro === "Por promedios (mejores notas a peores notas)") {
+      estudiantesLocalFiltrados = estudiantesLocal.sort((a, b) => b.promedio - a.promedio);
+      estudiantesSessionFiltrados = estudiantesSession.sort((a, b) => b.promedio - a.promedio);
+    } else if (filtro === "Por promedios (peores notas a mejores notas)") {
+      estudiantesLocalFiltrados = estudiantesLocal.sort((a, b) => a.promedio - b.promedio);
+      estudiantesSessionFiltrados = estudiantesSession.sort((a, b) => a.promedio - b.promedio);
+    }
+
+    estudiantesLocalFiltrados.forEach((estudiante) => {
+      localHTML += `<li>${estudiante.nombre} - Promedio: ${estudiante.promedio}</li>`;
+    });
+
+    estudiantesSessionFiltrados.forEach((estudiante) => {
+      sessionHTML += `<li>${estudiante.nombre} - Promedio: ${estudiante.promedio}</li>`;
+    });
+  }
+
+  localHTML += "</ul>";
+  sessionHTML += "</ul>";
+
+  // Mostrar contenido filtrado
+  localDataDiv.innerHTML = localHTML;
+  sessionDataDiv.innerHTML = sessionHTML;
+}
+
+ordenarMejoresBtn.addEventListener("click", filtrarData);
+ordenarPeoresBtn.addEventListener("click", filtrarData);
